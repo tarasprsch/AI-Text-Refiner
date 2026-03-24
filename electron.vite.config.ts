dff9d3f41cd2c09650b84ae66250112f178f3ee6
tarasprsch <laint2000@gmail.com>
2026-03-24 20:04:02 +0200

    optimization: config file

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,6 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'electron-vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   main: {},
@@ -9,7 +9,6 @@ export default defineConfig({
     root: resolve('src/renderer'),
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src'),
         '@shared': resolve('src/shared')
       }
     },
